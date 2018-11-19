203bf401acc55c7efdb87b46c7b7e1f2c6886fdd
Kritika Garg <iti.kritika@gmail.com>
2018-11-18 16:26:27 -0800

    Add files via upload

---
+++ howdy/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from howdy import views
+
+
+urlpatterns = [
+    url(r'^$', views.HomePageView.as_view()),
+    url(r'^about/$', views.AboutPageView.as_view()),
+]# -*- coding: utf-8 -*-
+
